13107a5af851f8fea0da1d352090dac55731bddb
oliver-kg <oliver.krums@gmail.com>
2026-04-02 12:18:23 +0100

    Day 6 - Torsion Angles Implemented
    Added Torsion angles which exist within molecules which have atoms that can have a length of 4, at the mid position, for example ethane which has the c-c bond which is in the middle of a h-c-c-h part of the molecule.
    All other forces still hold, and total energy is still conserved very well.

--- MD_Engine/MD_Engine.py
+++ MD_Engine/MD_Engine.py
@@ -36,6 +36,7 @@ class Bond:
         self.Ideal_Dist = Ideal_Dist
         self.K = K
 
+#bond angle class
 class Bond_Angle:
     def __init__(self, ideal_ang_deg, theta, first, second, third, k):
         self.ideal_ang = math.radians(ideal_ang_deg)
@@ -45,123 +46,115 @@ class Bond_Angle:
         self.third = third
         self.k = k
     
+# torsion angle class
+class Torsion_Angle:
+    def __init__(self, a1, a2, a3, a4, n, psi, delta_deg, k):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.n = n
+        self.psi = psi
+        self.delta = math.radians(delta_deg)
+        self.k = k
 
 atoms = []
 balls = []
 bonds = []
 bond_angles = []
+torsion_angles = []
 bond_visuals = []
+
 
 
 # create test bonds
 bonds = [
-    #methane
+    #propane
     Bond(0, 1, 1, 500),
-    Bond(0, 2, 1, 500),
+    Bond(1, 2, 1, 500),
+
     Bond(0, 3, 1, 500),
     Bond(0, 4, 1, 500),
+    Bond(0, 5, 1, 500),
 
-    #methane
-    Bond(5, 6, 1, 500),
-    Bond(5, 7, 1, 500),
-    Bond(5, 8, 1, 500),
-    Bond(5, 9, 1, 500),
+    Bond(1, 6, 1, 500),
+    Bond(1, 7, 1, 500),
 
-    #ethane
-    Bond(30, 31, 1, 500),
-    Bond(30, 32, 1, 500),
-    Bond(30, 33, 1, 500),
-    Bond(30, 34, 1, 500),
-    Bond(31, 35, 1, 500),
-    Bond(31, 36, 1, 500),
-    Bond(31, 37, 1, 500),
-
-    #water
-    Bond(38, 39, 1, 500),
-    Bond(39, 40, 1, 500)
-
+    Bond(2, 8, 1, 500),
+    Bond(2, 9, 1, 500),
+    Bond(2, 10, 1, 500),
 ]
 
 #create test angles
 bond_angles = [
-    
-    #methane
-    Bond_Angle(109.5, 0, 1, 0, 2, 200),
-    Bond_Angle(109.5, 0, 1, 0, 3, 200),
-    Bond_Angle(109.5, 0, 1, 0, 4, 200),
-    Bond_Angle(109.5, 0, 2, 0, 3, 200),
-    Bond_Angle(109.5, 0, 2, 0, 4, 200),
+    #propane
+    # around carbon 0
     Bond_Angle(109.5, 0, 3, 0, 4, 200),
+    Bond_Angle(109.5, 0, 3, 0, 5, 200),
+    Bond_Angle(109.5, 0, 4, 0, 5, 200),
+    Bond_Angle(109.5, 0, 3, 0, 1, 200),
+    Bond_Angle(109.5, 0, 4, 0, 1, 200),
+    Bond_Angle(109.5, 0, 5, 0, 1, 200),
 
-    #methane
-    Bond_Angle(109.5, 0, 6, 5, 7, 200),
-    Bond_Angle(109.5, 0, 6, 5, 8, 200),
-    Bond_Angle(109.5, 0, 6, 5, 9, 200),
-    Bond_Angle(109.5, 0, 7, 5, 8, 200),
-    Bond_Angle(109.5, 0, 7, 5, 9, 200),
-    Bond_Angle(109.5, 0, 8, 5, 9, 200),
+    # around carbon 1
+    Bond_Angle(109.5, 0, 0, 1, 2, 200),
+    Bond_Angle(109.5, 0, 0, 1, 6, 200),
+    Bond_Angle(109.5, 0, 0, 1, 7, 200),
+    Bond_Angle(109.5, 0, 2, 1, 6, 200),
+    Bond_Angle(109.5, 0, 2, 1, 7, 200),
+    Bond_Angle(109.5, 0, 6, 1, 7, 200),
 
-    #ethane
-    Bond_Angle(107.4, 0, 32, 30, 33, 200),
-    Bond_Angle(107.4, 0, 32, 30, 34, 200),
-    Bond_Angle(107.4, 0, 33, 30, 34, 200),
-    Bond_Angle(111, 0, 32, 30, 31, 200),
-    Bond_Angle(111, 0, 33, 30, 31, 200),
-    Bond_Angle(111, 0, 34, 30, 31, 200),
-    Bond_Angle(107.4, 0, 36, 31, 35, 200),
-    Bond_Angle(107.4, 0, 36, 31, 37, 200),
-    Bond_Angle(107.4, 0, 37, 31, 35, 200),
-    Bond_Angle(111, 0, 35, 31, 30, 200),
-    Bond_Angle(111, 0, 36, 31, 30, 200),
-    Bond_Angle(111, 0, 37, 31, 30, 200),
+    # around carbon 2
+    Bond_Angle(109.5, 0, 8, 2, 9, 200),
+    Bond_Angle(109.5, 0, 8, 2, 10, 200),
+    Bond_Angle(109.5, 0, 9, 2, 10, 200),
+    Bond_Angle(109.5, 0, 8, 2, 1, 200),
+    Bond_Angle(109.5, 0, 9, 2, 1, 200),
+    Bond_Angle(109.5, 0, 10, 2, 1, 200),
+    ]
 
-    #water
-    Bond_Angle(104.5, 0, 38, 39, 40, 200),
+torsion_angles = [
+    #propane
+    # across bond 0-1
+    Torsion_Angle(3, 0, 1, 2, 3, 0, 180, 5),
+    Torsion_Angle(3, 0, 1, 6, 3, 0, 180, 5),
+    Torsion_Angle(3, 0, 1, 7, 3, 0, 180, 5),
+
+    Torsion_Angle(4, 0, 1, 2, 3, 0, 180, 5),
+    Torsion_Angle(4, 0, 1, 6, 3, 0, 180, 5),
+    Torsion_Angle(4, 0, 1, 7, 3, 0, 180, 5),
+
+    Torsion_Angle(5, 0, 1, 2, 3, 0, 180, 5),
+    Torsion_Angle(5, 0, 1, 6, 3, 0, 180, 5),
+    Torsion_Angle(5, 0, 1, 7, 3, 0, 180, 5),
+
+    # across bond 1-2
+    Torsion_Angle(0, 1, 2, 8, 3, 0, 180, 5),
+    Torsion_Angle(0, 1, 2, 9, 3, 0, 180, 5),
+    Torsion_Angle(0, 1, 2, 10, 3, 0, 180, 5),
+
+    Torsion_Angle(6, 1, 2, 8, 3, 0, 180, 5),
+    Torsion_Angle(6, 1, 2, 9, 3, 0, 180, 5),
+    Torsion_Angle(6, 1, 2, 10, 3, 0, 180, 5),
+
+    Torsion_Angle(7, 1, 2, 8, 3, 0, 180, 5),
+    Torsion_Angle(7, 1, 2, 9, 3, 0, 180, 5),
+    Torsion_Angle(7, 1, 2, 10, 3, 0, 180, 5),
     ]
 
 #create test molecules
 molecules = [
-    #methane
-    Molecule([0,1,2,3,4]),
-    
-    #methane
-    Molecule([5,6,7,8,9]),
-    
-    #ethane
-    Molecule([30,31,32,33,34,35,36,37]),
+    #propane
+    Molecule([0,1,2,3,4,5,6,7, 8, 9, 10]),
 
-    #water
-    Molecule([38,39,40]),
-
-    #singles
-    Molecule([10]),
-    Molecule([11]),
-    Molecule([12]),
-    Molecule([13]),
-    Molecule([14]),
-    Molecule([15]),
-    Molecule([16]),
-    Molecule([17]),
-    Molecule([18]),
-    Molecule([19]),
-    Molecule([20]),
-    Molecule([21]),
-    Molecule([22]),
-    Molecule([23]),
-    Molecule([24]),
-    Molecule([25]),
-    Molecule([26]),
-    Molecule([27]),
-    Molecule([28]),
-    Molecule([29])
     ]
 
                 # simulation pararmeters
 e_pot = 0
 k_e = 0
 e_total = 0
-no_balls = 41
-dt = 0.001
+no_balls = 11
+dt = 0.0008
 sigma = 0.8
 epsilon = 0.2
 PBC_box_length = 10
@@ -208,25 +201,18 @@ for a in atoms:
     a.pos.y -= (n_per_axis * spacing) / 2
     a.pos.z -= (n_per_axis * spacing) / 2
 
-
-# change masses test
-atoms[0].mass = 12
-atoms[5].mass = 12
-atoms[30].mass = 12
-atoms[31].mass = 12
-atoms[39].mass = 16
-
 # create balls
 for a in atoms:
     b = sphere(pos=a.pos, radius=0.1, color=vector(0,0,0), make_trail=False)
     balls.append(b)
 
-# change radius test
+
+atoms[0].mass = 12
+atoms[1].mass = 12
+atoms[2].mass = 12
 balls[0].radius = 0.2
-balls[5].radius = 0.2
-balls[30].radius = 0.2
-balls[31].radius = 0.2
-balls[39].radius = 0.22
+balls[1].radius = 0.2
+balls[2].radius = 0.2
 
 # Lennard Jones Force Equation (VDW's)
 def Calc_LJ(dist, direction):
@@ -345,8 +331,8 @@ def Calc_Physics():
         atoms[atomB].force += f_b
         atoms[atomC].force += f_c
 
-        e_pot += 0.5 * k * (theta - ideal_ang)**2                               # update the potential energy change from the angle
-
+        e_pot += 0.5 * k * (theta - ideal_ang)**2                # update the potential energy change from the angle
+        
     # bond forces
     for bond in bonds:
         a = bond.a1
@@ -372,7 +358,64 @@ def Calc_Physics():
 
         e_pot += 0.5 * k * (r - r0)**2          # calc the harmonic bond potential
 
-    # LJ for nonbonded pairs only
+    
+     
+    # torsion angles
+    for angle in torsion_angles:
+        a1 = angle.a1                           # 4 atoms involved in the torsion bond
+        a2 = angle.a2
+        a3 = angle.a3
+        a4 = angle.a4
+        n = angle.n                             # number of stable positions per turn
+        psi = angle.psi                         # the angle of the bond
+        delta = angle.delta                     # offset of graph
+        k = angle.k                             # how strong the torsion is
+
+        b1 = atoms[a2].pos - atoms[a1].pos      # directions of the three bonds
+        b2 = atoms[a3].pos - atoms[a2].pos
+        b3 = atoms[a4].pos - atoms[a3].pos
+
+        n1 = cross(b1, b2)                      # normal to plane ABC
+        n2 = cross(b2, b3)                      # normal to plane BCD
+
+        eps = 1e-12
+        n1_sq = dot(n1, n1)
+        n2_sq = dot(n2, n2)
+        b2_sq = dot(b2, b2)
+        b2_mag = mag(b2)
+
+        if n1_sq < eps or n2_sq < eps or b2_sq < eps:    # makes sure small values dont blow the system up
+            continue
+        
+        x = dot(n1, n2)
+        y = dot(cross(n1, n2), norm(b2))
+
+        psi = math.atan2(y, x)                  # caluclate current psi angle
+        angle.psi = psi 
+
+        dV_dpsi = -k*n*math.sin(n*psi-delta)            # how strongly the torsion wants to rotate
+        fa_pref = -dV_dpsi * (b2_mag / n1_sq)           # calculates the geometric scallings of the force
+        fd_pref =  dV_dpsi * (b2_mag / n2_sq)
+
+        f_a = fa_pref * n1                              # aligning the forces with the direction to the plane
+        f_d = fd_pref * n2
+
+        c1 = dot(b1, b2) / b2_sq                        # calculates how much b and c lean along the middle bond
+        c2 = dot(b3, b2) / b2_sq
+
+        f_b = -f_a + c1 * f_a - c2 * f_d                # calculate the final forces of b and c, by taking into account the forces of a and d
+        f_c = -f_d - c1 * f_a + c2 * f_d
+
+        atoms[a1].force += f_a                         # apply force to atom a
+        atoms[a2].force += f_b                         # apply force to atom b
+        atoms[a3].force += f_c                         # apply force to atom c
+        atoms[a4].force += f_d                         # apply force to atom d
+
+        
+        e_pot += k*(1+math.cos((n*psi)-delta))  # calculate the potential energy
+        
+
+
     cutoff = 2.5 * sigma                        # compute LJ forces only within a cutoff reigon, as forces are too weak anyway
     U_shift = 4 * epsilon * ((sigma / cutoff)**12 - (sigma / cutoff)**6) # used so the LJ potential smoothly becomes 0 instead of cutting off
 
@@ -407,7 +450,7 @@ e_pot = Calc_Physics()
 
 step = 0
 relaxing = True 
-relax_steps = 3500
+relax_steps = 8000
 E0 = None
 while True:
     rate(120)                                                       # capped at 120 render
@@ -463,9 +506,13 @@ while True:
                 PBC_Box_For_Vectors(r_vec)
                 print(f"Bond {a}-{b}: {mag(r_vec):.6f}")
                 
-            for angles in bond_angles:
-                print(f"Angle: {math.degrees(angles.theta)}")
+            for angle in bond_angles:
+                print(f"Angle: {math.degrees(angle.theta)}")
 
+            for angle in torsion_angles:
+                print(f"Angle: {math.degrees(angle.psi)}")
+
+        
     # update graphics once per frame
     for i in range(no_balls):
         balls[i].pos = atoms[i].pos                                 # moves balls to current pos
